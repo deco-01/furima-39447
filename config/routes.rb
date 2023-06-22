97f746baad6a68e1ecd1e1889dc20fb122aa81a8
deco-01 <ex.k.futatunohoshino@gmail.com>
2023-06-22 20:58:56 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:edit, :update]
-  resources :items, only: [:new, :create, :index]
+  resources :items, only: [:new, :create, :index, :show, :edit, :destroy]
 
   
 
